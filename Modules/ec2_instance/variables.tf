1f1d8eb27560be2e240b4493da745f4d8d382aaf
Rohan Rama Poojari <123969188+Cetyl@users.noreply.github.com>
2024-09-06 12:01:07 +0200

    Create variables.tf

---
+++ Modules/ec2_instance/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+    description = "value for ami"
+  }
+
+variable "instance_type_value" {
+    description = "value for instance type"
+  }
+
+variable "region_value" {
+    description = "value for region"
+  }
+
